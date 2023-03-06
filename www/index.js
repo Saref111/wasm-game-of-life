08afb76d953509ec95ddfc5ba40134d5d7ed64bc
Saref111 <saref012@gmail.com>
2023-03-06 15:17:56 +0200

    added frontend to main repo

---
+++ www/index.js
@@ -0,0 +1,16 @@
+import { Universe } from "wasm-game-of-life";
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
+
+
+// Path: www\index.html
